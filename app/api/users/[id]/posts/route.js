5b1490515e904e6add6c5c2a45962cd5028d22da
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-05-18 01:31:26 +0200

    🎉 Add : routes file prompt by user

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connectToDB } from '@utils/database_conn';
+import Prompt from '@models/prompt';
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({
+      creator: params.id,
+    }).populate('creator');
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to fetch all prompts.', { status: 500 });
+  }
+};
